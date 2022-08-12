690ce7a15f9e630fccd9c867e4bd73325c53b1d1
bikramghisoani <bikramghi@soanitech.com>
2022-08-12 16:14:59 +0545

    setting up chakraui/react-query

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is the react application</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <p>this is the main page</p>
+    </div>
+  );
+};
+
+export default MainPage;
